10839fbeb0e03942c0a2b45dce23b321a714eaeb
mevine <atienovivian649@gmail.com>
2022-02-11 18:42:00 +0300

    second commit

--- Register.php
+++ Register.php
@@ -1,12 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-<body>
-    
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-</body>
-</html>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  if (empty($_POST["telephone"])) {
+      $telephoneErr ="phone number required";
+    
+  }else{
+      $telephone =test_input($_POST["telephone"]);
+  }
+
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+?>
--- formvalidation.php
+++ formvalidation.php
@@ -1,31 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<style>
+.error {color: #FF0000;}
+body{
+  background-color: lightblue;
+}
+  
+
+</style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formvalidation</title>
 </head>
 <body>
+<?php
+    //  define variables  and empty values
+    $fnameErr = $eemailErr = $genderErr = $websiteErr = $telephoneErr="";
+    $fname = $eemail = $gender = $comment = $website = $telephone="";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+          $fnameErr = "Name is required";
+        } else {
+          $fname = test_input($_POST["name"]);
+        }
+        
+        if (empty($_POST["email"])) {
+          $eemailErr = "Email is required";
+        } else {
+          $eemail = test_input($_POST["email"]);
+        }
+        if (empty($_POST["number"])) {
+            $telephoneErr ="phonenumber required";
+        }else {
+            $telephone = test_input($_POST["number"]);
+        }
+          
+        if (empty($_POST["website"])) {
+          $website = "valid url";
+        } else {
+          $website = test_input($_POST["website"]);
+        }
+      
+        if (empty($_POST["Comment"])) {
+          $comment = "";
+        } else {
+          $comment = test_input($_POST["comment"]);
+        }
+      
+        if (empty($_POST["gender"])) {
+          $genderErr = "Gender is required";
+        } else {
+          $gender = test_input($_POST["gender"]);
+        }
+      }
+      
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+
+    ?>
     <h2>my form validation</h2>
-    <form action="" method="post">
+    <p><span class="error">* required field</span></p>
+    <form action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"><br>
     <label for="fname">fullname</label><br>
+    <span class="error">* <?php echo $fnameErr;?></span><br>
     <input type="text" id="fname" name="name"><br>
+    
     <label for="email">email adress</label><br>
-    <input type="email" id="eeemail" name="email"><br>
+    <span class="error">* <?php echo $eemailErr;?></span><br>
+    <input type="email" id="eemail" name="email"><br>
+   
     <label for="telephone">phone number</label><br>
+    <span class="error">* <?php echo $telephoneErr;?></span><br>
     <input type="text" id="telephone"name="number"><br>
+    
     <label for="website">Website</label><br>
+    <span class="error">* <?php echo $websiteErr;?></span>
     <input type="text" id="website" name="website"><br>
+    
+    <label for="comment">Comments</label><br>
     <textarea name="Comment" id="comment" cols="30" rows="10"></textarea><br>
     <br><br>
     Gender:
+    <span class="error">* <?php echo $genderErr;?></span>
     <input type="radio"name="gender" value="female">Female
     <input type="radio" name="gender"value="male">Male
     <input type="radio" name="gender"value="other">Other<br>
+    
     <br>
     <input type="submit" value="Register"><br>
     </form>
 
+    <?php
+echo "<h2>Your Input:</h2>";
+echo $fname;
+echo "<br>";
+echo $eemail;
+echo "<br>";
+echo $telephone;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
     
 
 </body>
